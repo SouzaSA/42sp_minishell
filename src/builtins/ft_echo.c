7712ab15fc6258d7ab2a15e4df012b30648fd632
Edmar Paulino <edmarpaulino9@gmail.com>
2021-12-18 16:58:37 -0300

    add ft_echo function

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -3,10 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 10:51:42 by sde-alva          #+#    #+#             */
-/*   Updated: 2021/12/18 10:51:43 by sde-alva         ###   ########.fr       */
+/*   Updated: 2021/12/18 16:56:14 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_minishell.h"
+
+void	ft_echo(char *str, int flag)
+{
+	if (flag == 0)
+		ft_putendl_fd(str, 1);
+	else
+		ft_putstr_fd(str, 1);
+}
