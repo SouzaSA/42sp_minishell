877bb351fb0a7f1268f338450bfd92070edc7833
sde-alva <sde-alva@student.42sp.org.br>
2022-01-15 18:49:11 -0300

    update push and pop

--- libft/src/lst/ft_lstpop.c
+++ libft/src/lst/ft_lstpop.c
@@ -1,14 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstpop.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/15 10:12:26 by sde-alva          #+#    #+#             */
+/*   Updated: 2022/01/15 18:48:32 by sde-alva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	*ft_lstpop(t_list **lst, void (*del)(void *))
+void	*ft_lstpop(t_list **lst)
 {
+	t_list	*node;
 	void	*content;
 
 	content = NULL;
-	if (*lst)
+	if (lst && *lst)
 	{
+		node = (*lst)->next;
 		content = (*lst)->content;
-		ft_lstdel_front(lst, del);
+		free(*lst);
+		*lst = node;
 	}
 	return (content);
 }
