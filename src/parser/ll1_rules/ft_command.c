c9f25646fef228f05c19861f8a38a5c26c8f2f03
sde-alva <sde-alva@student.42sp.org.br>
2022-01-21 08:44:19 -0300

    fixed error on command ll1 rule

--- src/parser/ll1_rules/ft_command.c
+++ src/parser/ll1_rules/ft_command.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 11:03:04 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/01/20 09:44:07 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/01/21 08:41:36 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	ft_command(t_list **symbol_stack, enum e_symbol tok_type)
 {
 	if (tok_type >= 0)
 	{
+		free(ft_lstpop(symbol_stack));
 		if (tok_type == TS_LBRACE)
 		{
-			free(ft_lstpop(symbol_stack));
 			ft_lstpush(symbol_stack, ft_new_symbol(NTS_COMMAND1));
 			ft_lstpush(symbol_stack, ft_new_symbol(NTS_SUBSHELL));
 		}
