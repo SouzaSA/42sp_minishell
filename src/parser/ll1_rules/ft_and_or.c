4ceb79bd420a05a07d79f9e74b15cabeede6cd4c
sde-alva <sde-alva@student.42sp.org.br>
2022-01-26 21:19:53 -0300

    ast hell

--- src/parser/ll1_rules/ft_and_or.c
+++ src/parser/ll1_rules/ft_and_or.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 10:58:53 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/01/26 12:42:38 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/01/26 21:19:31 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,43 @@
 
 void	ft_and_or(t_syntax *stx_vars, enum e_symbol tok_type)
 {
-	t_stk_node	*node;
-	t_stk_node	*node_old;
+	t_stk_node	*stk_node;
+	t_stk_node	*stk_node_old;
 
 	if (tok_type >= 0)
 	{
-		node_old = (t_stk_node *)ft_lstpop(&stx_vars->symbol_stack);
-		node_old->ast_node->children++;
-		node_old->ast_node->first_child = ft_new_node(AST_PIPE);
-		node = (t_stk_node *)ft_stk_node(NTS_AND_OR1);
-		node->ast_node = node_old->ast_node;
-		ft_lstpush(&stx_vars->symbol_stack, node);
-		node = (t_stk_node *)ft_stk_node(NTS_PIPELINE);
-		node->ast_node = node_old->ast_node->first_child;
-		ft_lstpush(&stx_vars->symbol_stack, node);
-		free(node_old);
+		stk_node_old = (t_stk_node *)ft_lstpop(&stx_vars->symbol_stack);
+		stk_node_old->ast_node->children++;
+		stk_node_old->ast_node->type = AST_TMP;
+		stk_node_old->ast_node->first_child = ft_new_node(AST_PIPE);
+		stk_node = (t_stk_node *)ft_stk_node(NTS_AND_OR1);
+		stk_node->ast_node = stk_node_old->ast_node;
+		ft_lstpush(&stx_vars->symbol_stack, stk_node);
+		stk_node = (t_stk_node *)ft_stk_node(NTS_PIPELINE);
+		stk_node->ast_node = stk_node_old->ast_node->first_child;
+		ft_lstpush(&stx_vars->symbol_stack, stk_node);
+		free(stk_node_old);
 	}
 }
 
 void	ft_and_or1(t_syntax *stx_vars, enum e_symbol tok_type)
 {
-	t_stk_node	*node;
-	t_stk_node	*node_old;
+	t_stk_node	*stk_node;
+	t_stk_node	*stk_node_old;
 
 	if (tok_type >= 0)
 	{
-		node_old = (t_stk_node *)ft_lstpop(&stx_vars->symbol_stack);
+		stk_node_old = (t_stk_node *)ft_lstpop(&stx_vars->symbol_stack);
 		if (tok_type == TS_AND_IF)
-			node_old->ast_node->type = AST_AND;
+			stk_node_old->ast_node->type = AST_AND;
 		else
-			node_old->ast_node->type = AST_OR;
-		node_old->ast_node->children++;
-		node_old->ast_node->first_child->next_sibling = ft_new_node(AST_TMP);
-		node = (t_stk_node *)ft_stk_node(NTS_AND_OR);
-		node->ast_node = node_old->ast_node->first_child->next_sibling;
-		ft_lstpush(&stx_vars->symbol_stack, node);
+			stk_node_old->ast_node->type = AST_OR;
+		stk_node_old->ast_node->children++;
+		stk_node_old->ast_node->first_child->next_sibling = ft_new_node(AST_TMP);
+		stk_node = (t_stk_node *)ft_stk_node(NTS_AND_OR);
+		stk_node->ast_node = stk_node_old->ast_node->first_child->next_sibling;
+		ft_lstpush(&stx_vars->symbol_stack, stk_node);
 		ft_lstpush(&stx_vars->symbol_stack, ft_stk_node(tok_type));
-		free(node_old);
+		free(stk_node_old);
 	}
 }
--- src/parser/ll1_rules/ft_pipeline.c
+++ src/parser/ll1_rules/ft_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 11:01:54 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/01/25 15:22:36 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/01/26 21:19:29 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,40 @@
 
 void	ft_pipeline(t_syntax *stx_vars, enum e_symbol tok_type)
 {
+	t_stk_node	*stk_node;
+	t_stk_node	*stk_node_old;
+
 	if (tok_type >= 0)
 	{
-		free(ft_lstpop(&stx_vars->symbol_stack));
-		ft_lstpush(&stx_vars->symbol_stack, ft_stk_node(NTS_PIPELINE1));
-		ft_lstpush(&stx_vars->symbol_stack, ft_stk_node(NTS_COMMAND));
+		stk_node_old = (t_stk_node *)ft_lstpop(&stx_vars->symbol_stack);
+		stk_node_old->ast_node->children++;
+		stk_node_old->ast_node->type = AST_TMP;
+		stk_node_old->ast_node->first_child = ft_new_node(NTS_COMMAND);
+		stk_node = (t_stk_node *)ft_stk_node(NTS_PIPELINE1);
+		stk_node->ast_node = stk_node_old->ast_node;
+		ft_lstpush(&stx_vars->symbol_stack, stk_node);
+		stk_node = (t_stk_node *)ft_stk_node(NTS_COMMAND);
+		stk_node->ast_node = stk_node_old->ast_node->first_child;
+		ft_lstpush(&stx_vars->symbol_stack, stk_node);
+		free(stk_node_old);
 	}
 }
 
 void	ft_pipeline1(t_syntax *stx_vars, enum e_symbol tok_type)
 {
+	t_stk_node	*stk_node;
+	t_stk_node	*stk_node_old;
+
 	if (tok_type >= 0)
 	{
-		free(ft_lstpop(&stx_vars->symbol_stack));
+		stk_node_old = (t_stk_node *)ft_lstpop(&stx_vars->symbol_stack);
+		stk_node_old->ast_node->type = AST_PIPE;
+		stk_node_old->ast_node->children++;
+		stk_node_old->ast_node->first_child->next_sibling = ft_stk_node(NTS_PIPELINE);
+		stk_node = (t_stk_node *)ft_stk_node(tok_type);
+		stk_node->ast_node = stk_node_old->ast_node->first_child->next_sibling;
 		ft_lstpush(&stx_vars->symbol_stack, ft_stk_node(NTS_PIPELINE));
 		ft_lstpush(&stx_vars->symbol_stack, ft_stk_node(tok_type));
+		free(stk_node_old);
 	}
 }
