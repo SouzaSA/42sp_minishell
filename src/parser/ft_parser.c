97f54b970a7e47053c977e68b2a80b4a9ac33931
sde-alva <sde-alva@student.42sp.org.br>
2022-02-04 11:08:29 -0300

    do a function to print AST

--- src/parser/ft_parser.c
+++ src/parser/ft_parser.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 14:18:16 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/02/03 19:50:23 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/02/04 11:01:27 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ t_ast	*ft_parser(char *line, void	(***tt)(t_list **, enum e_symbol))
 
 static t_ast	*ft_syntax(t_list *toks, void (***tt)(t_list **, enum e_symbol))
 {
-	t_ast			*ast;
-	t_list			*symbol_stack;
-	void			(*production)(t_list **, enum e_symbol);
-	enum e_symbol	*symbol;
-	int				is_valid;
+	t_ast		*ast;
+	t_list		*symbol_stack;
+	void		(*production)(t_list **, enum e_symbol);
+	t_stk_node	*stk_node;
+	int			is_valid;
 
 	ast = ft_new_node(AST_TMP);
 	symbol_stack = NULL;
@@ -45,19 +45,31 @@ static t_ast	*ft_syntax(t_list *toks, void (***tt)(t_list **, enum e_symbol))
 	is_valid = 1;
 	while (is_valid && ft_lstsize(symbol_stack) > 0)
 	{
-		symbol = (enum e_symbol *)ft_lsttop(symbol_stack);
-		if (((t_token *)toks->content)->tok_type == *symbol)
+		stk_node = (t_stk_node *)ft_lsttop(symbol_stack);
+		if (((t_token *)toks->content)->tok_type == stk_node->stk_type)
 		{
+			if ((stk_node->ast_type == AST_CMD || stk_node->ast_type == AST_ASSIGN || stk_node->ast_type == AST_REDIR) && stk_node->ast_node && !stk_node->ast_node->cmd)
+			{
+				stk_node->ast_node->cmd = (t_command *)malloc(sizeof(t_command));
+				stk_node->ast_node->cmd->cmd = NULL;
+				stk_node->ast_node->cmd->redir = NULL;
+				stk_node->ast_node->cmd->assign = NULL;
+			}
+			if (stk_node->ast_type == AST_CMD)
+				ft_lstadd_back(&stk_node->ast_node->cmd->cmd, ft_lstnew(((t_token *)toks->content)->text));
+			if (stk_node->ast_type == AST_ASSIGN)
+				ft_lstadd_back(&stk_node->ast_node->cmd->assign, ft_lstnew(((t_token *)toks->content)->text));
+			if (stk_node->ast_type == AST_REDIR)
+				ft_lstadd_back(&stk_node->ast_node->cmd->redir, ft_lstnew(((t_token *)toks->content)->text));
 			toks = toks->next;
-			//add to ast
 			free(ft_lstpop(&symbol_stack));
 		}
 		else
 		{
 			//look at production rules to create node of the tree.
 			production = NULL;
-			if (*symbol < NUM_NTS)
-				production = tt[*symbol][((t_token *)toks->content)->tok_type - NUM_NTS];
+			if (stk_node->stk_type < NUM_NTS)
+				production = tt[stk_node->stk_type][((t_token *)toks->content)->tok_type - NUM_NTS];
 			if (production)
 				production(&symbol_stack, ((t_token *)toks->content)->tok_type);
 			else
