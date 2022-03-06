f301651047ab1558ee191eb03ad4b81b03c2872e
Edmar Paulino <edmarpaulino9@gmail.com>
2022-03-06 14:42:55 -0300

    change ft_handle_prompt_signals to use sigaction instead signal

--- src/signals/ft_handle_prompt_signals.c
+++ src/signals/ft_handle_prompt_signals.c
@@ -6,25 +6,45 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 19:56:26 by edpaulin          #+#    #+#             */
-/*   Updated: 2022/03/05 14:31:38 by edpaulin         ###   ########.fr       */
+/*   Updated: 2022/03/06 12:20:01 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_signals.h"
 
-void	ft_prompt_cancel(int signal);
+static void	ft_handle_sigint(void);
+static void	ft_prompt_cancel(int signal);
+static void	ft_handle_sigquit(void);
 
 void	ft_handle_prompt_signals(void)
 {
-	signal(SIGINT, &ft_prompt_cancel);
-	signal(SIGQUIT, SIG_IGN);
+	ft_handle_sigint();
+	ft_handle_sigquit();
 }
 
-void	ft_prompt_cancel(int signal)
+static void	ft_handle_sigint(void)
 {
-	g_exit_status = 128 + signal;
+	struct sigaction	sa;
+
+	sa.sa_handler = &ft_prompt_cancel;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+}
+
+static void	ft_prompt_cancel(int signal)
+{
+	g_exit_status = (128 + signal);
 	write(1, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
+}
+
+static void	ft_handle_sigquit(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = SIG_IGN;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGQUIT, &sa, NULL);
 }
