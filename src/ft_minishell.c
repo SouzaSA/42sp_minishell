f02710e5560e55d1c76d14b84cd3c076a3618480
sde-alva <sde-alva@student.42sp.org.br>
2022-03-10 16:52:51 -0300

    changed ctrl-d behavior on cmdline, and only spaces line

--- src/ft_minishell.c
+++ src/ft_minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_minishell.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 13:36:17 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/03/06 16:14:48 by edpaulin         ###   ########.fr       */
+/*   Updated: 2022/03/10 16:50:36 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 #include <curses.h>
 #include <term.h>
 
+static void	ft_no_line(t_shell *shell);
+static int	ft_lineck(char *line);
 static void	ft_free_line(t_shell *shell);
-static void	ft_no_line(void);
 
 int	ft_minishell(char **envp)
 {
-	t_shell		shell;
 	char		*prompt;
+	t_shell		shell;
 
 	ft_init_minishell(&shell, envp);
 	while (1)
@@ -31,8 +32,8 @@ int	ft_minishell(char **envp)
 		shell.line = readline(prompt);
 		free(prompt);
 		if (!shell.line)
-			ft_no_line();
-		if (shell.line && ft_strlen(shell.line) == 0)
+			ft_no_line(&shell);
+		if (shell.line && (ft_strlen(shell.line) == 0 || ft_lineck(shell.line)))
 		{
 			ft_free_line(&shell);
 			continue ;
@@ -47,14 +48,29 @@ int	ft_minishell(char **envp)
 	return (0);
 }
 
+static void	ft_no_line(t_shell *shell)
+{
+	rl_clear_history();
+	ft_destroy_shell(shell);
+	write(1, "\n", 1);
+	exit(0);
+}
+
+static int	ft_lineck(char *line)
+{
+	int		has_any;
+	char	*trimmed;
+
+	trimmed = ft_strtrim(line, " ");
+	has_any = 0;
+	if (!trimmed || ft_strlen(trimmed) == 0)
+		has_any = 1;
+	free(trimmed);
+	return (has_any);
+}
+
 static void	ft_free_line(t_shell *shell)
 {
 	free(shell->line);
 	shell->line = NULL;
-}
-
-static void	ft_no_line(void)
-{
-	write(1, "\n", 1);
-	exit(0);
 }
