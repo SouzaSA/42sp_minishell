d4b5e7ad4285304d26076f5644107a808f4ece05
Edmar Paulino <edmarpaulino9@gmail.com>
2022-03-12 19:03:37 -0300

    fix expand star from the first directory

--- src/expand/ft_expand_star.c
+++ src/expand/ft_expand_star.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_expand_star.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 17:50:25 by edpaulin          #+#    #+#             */
-/*   Updated: 2022/03/11 14:40:30 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/03/12 19:02:47 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static void		ft_couple(t_list **list, t_list *prev, t_list *exp, t_list **n);
 static t_list	*ft_expand(char *str);
+static void		ft_expand_from_first_dir(t_list **list, t_list *split);
 
 void	ft_expand_star(t_list **list)
 {
@@ -71,10 +72,38 @@ static t_list	*ft_expand(char *str)
 		return (NULL);
 	fl = (t_file *)split->content;
 	list = NULL;
-	if (!split->next)
+	if (ft_lstsize(split) > 1 && ft_strchr(fl->name, '*'))
+		ft_expand_from_first_dir(&list, split);
+	else if (!split->next)
 		ft_add_expansion_to_list(&list, split, NULL);
 	else
 		ft_expand_list(&list, split, fl->name);
 	ft_lstclear(&split, &ft_del_content_file_struct);
 	return (list);
+}
+
+static void	ft_expand_from_first_dir(t_list **list, t_list *split)
+{
+	DIR				*folder;
+	struct dirent	*entry;
+	char			*pattern;
+
+	folder = opendir(".");
+	if (!folder)
+		return ;
+	entry = readdir(folder);
+	pattern = ft_strdup(((t_file *)split->content)->name);
+	while (entry)
+	{
+		if (ft_match_star(pattern, entry->d_name) \
+		&& ft_is_dir(entry->d_type))
+		{
+			free(((t_file *)split->content)->name);
+			((t_file *)split->content)->name = ft_strdup(entry->d_name);
+			ft_expand_list(list, split, ((t_file *)split->content)->name);
+		}
+		entry = readdir(folder);
+	}
+	free(pattern);
+	closedir(folder);
 }
