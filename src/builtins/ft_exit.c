9640b035ec374c0fe168a655c8c885214288bea7
sde-alva <sde-alva@student.42sp.org.br>
2022-03-13 10:34:23 -0300

    fixed a bug on exit status

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 10:51:26 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/03/05 15:51:37 by edpaulin         ###   ########.fr       */
+/*   Updated: 2022/03/13 10:29:09 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ static int	ft_exit_messages(int code, char *msg)
 
 	if (code == 0)
 	{
-		rtn = 0;
+		rtn = g_exit_status;
 		ft_putendl_fd("exit", 2);
 	}
 	else if (code == 1)
