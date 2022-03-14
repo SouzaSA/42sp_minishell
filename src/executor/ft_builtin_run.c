81404170aff177284fe1476eef26332fc0a7156d
sde-alva <sde-alva@student.42sp.org.br>
2022-03-14 14:39:34 -0300

    update builtin run behavior

--- src/executor/ft_builtin_run.c
+++ src/executor/ft_builtin_run.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 20:40:18 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/03/13 15:14:21 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/03/14 14:36:26 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ static int	ft_forker_builtin(t_shell *shell, t_cmd_data *data, t_cmd_blk *cmd)
 		data->forked = 1;
 	if (rtn == 0 && data->pid == 0)
 		rtn = ft_child(shell, data, cmd);
-	waitpid(-1, NULL, WNOHANG);
+	waitpid(-1, NULL, 0);
 	close(data->pipe_fd[1]);
 	return (rtn);
 }
@@ -79,7 +79,6 @@ static int	ft_child(t_shell *shell, t_cmd_data *data, t_cmd_blk *blk)
 			exit(0);
 	}
 	ft_handle_parent_process_signals();
-	waitpid(-1, NULL, WNOHANG);
 	close(data->pipe_fd[1]);
 	return (rtn);
 }
