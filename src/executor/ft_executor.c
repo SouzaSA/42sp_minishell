592b6fd64b6d4dd74a2a756ec4622c5111f2165a
Edmar Paulino <edmarpaulino9@gmail.com>
2022-03-17 11:01:16 -0300

    revert sigusr signal handler tests

--- src/executor/ft_executor.c
+++ src/executor/ft_executor.c
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 11:45:35 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/03/17 10:36:44 by edpaulin         ###   ########.fr       */
+/*   Updated: 2022/03/17 10:57:17 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,6 @@ static int	ft_run_cmds(t_shell *shell, t_list **cmd_stk)
 	if ((num_cmds > 1 || !cmd_data.builtin_flag) && cmd_data.forked)
 	{
 		waitpid(cmd_data.pid, &rtn, 0);
-		kill(cmd_data.pid, SIGUSR1);
 		if (!WIFSIGNALED(rtn))
 			g_exit_status = WEXITSTATUS(rtn);
 	}
--- src/signals/ft_handle_child_process_signals.c
+++ src/signals/ft_handle_child_process_signals.c
@@ -6,23 +6,20 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 14:45:19 by edpaulin          #+#    #+#             */
-/*   Updated: 2022/03/17 10:35:03 by edpaulin         ###   ########.fr       */
+/*   Updated: 2022/03/17 11:00:07 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_signals.h"
 
 static void	ft_handle_sigint(void);
-static void	ft_handle_sigquit(void);
 static void	ft_process_cancel(int signal);
-static void	ft_handle_sigusr1(void);
-static void	ft_process_kill(int signal);
+static void	ft_handle_sigquit(void);
 
 void	ft_handle_child_process_signals(void)
 {
 	ft_handle_sigint();
 	ft_handle_sigquit();
-	ft_handle_sigusr1();
 }
 
 static void	ft_handle_sigint(void)
@@ -48,20 +45,4 @@ static void	ft_handle_sigquit(void)
 	sa.sa_flags = SA_RESTART;
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGQUIT, &sa, NULL);
-}
-
-static void	ft_handle_sigusr1(void)
-{
-	struct sigaction	sa;
-
-	sa.sa_handler = &ft_process_kill;
-	sa.sa_flags = SA_RESTART;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1, &sa, NULL);
-}
-
-static void	ft_process_kill(int signal)
-{
-	if (signal)
-		exit(0);
 }
