04ce0ff097f867d2cdce2d20bf59d0cc7cb457e3
sde-alva <sde-alva@student.42sp.org.br>
2022-03-26 20:06:55 +0100

    removed extra space from ft_executor.h and update exit status

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 10:51:26 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/03/15 16:53:13 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/03/26 20:03:57 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,12 @@ int	ft_exit(t_shell *shell, t_cmd_data *data, t_list *cmds, t_cmd_blk *blk)
 		rtn = ft_exit_messages(ft_atoi((char *)cmds->next->content), NULL, 0);
 	else
 		rtn = ft_exit_messages(g_exit_status, NULL, 0);
-	if (rtn != 2)
-	{
-		if (ft_lstsize(*data->cmd_stk))
-			ft_destroy_ast_stk(data->cmd_stk);
-		ft_destroy_command(&blk);
-		rl_clear_history();
-		ft_destroy_shell(shell);
-		exit(g_exit_status);
-	}
-	return (g_exit_status);
+	if (ft_lstsize(*data->cmd_stk))
+		ft_destroy_ast_stk(data->cmd_stk);
+	ft_destroy_command(&blk);
+	rl_clear_history();
+	ft_destroy_shell(shell);
+	exit(g_exit_status);
 }
 
 static int	ft_is_num(char *num)
@@ -83,6 +79,7 @@ static int	ft_exit_messages(int code, char *msg, int flag)
 
 static void	ft_put_error(char *msg, int flag)
 {
+	ft_putendl_fd("exit", 2);
 	if (flag == 1)
 	{
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
@@ -92,9 +89,5 @@ static void	ft_put_error(char *msg, int flag)
 		ft_putstr_fd("minishell: exit: ", 2);
 		ft_putstr_fd(msg, 2);
 		ft_putendl_fd(": numeric argument required", 2);
-	}
-	else
-	{
-		ft_putendl_fd("exit", 2);
-	}
+	}	
 }
